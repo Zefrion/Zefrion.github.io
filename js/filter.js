321b76386dc1349198da53179b967c73c91386d8
SonoNessunk <91794916+SonoNessunk@users.noreply.github.com>
2023-11-22 10:03:36 +0100

    .

---
+++ js/filter.js
@@ -0,0 +1,19 @@
+  // Funzione per filtrare la lista
+  function filterList() {
+    var select, ul, li, category;
+    select = document.getElementById('categorySelect');
+    ul = document.getElementById('myList');
+    li = ul.getElementsByTagName('div');
+    category = select.value;
+
+    // Mostra o nascondi gli elementi in base alla categoria selezionata
+    for (var i = 0; i < li.length; i++) {
+      var dataCategory = li[i].getAttribute('data-category');
+
+      if (category === 'tutte' || dataCategory === category) {
+        li[i].style.display = '';
+      } else {
+        li[i].style.display = 'none';
+      }
+    }
+  }
