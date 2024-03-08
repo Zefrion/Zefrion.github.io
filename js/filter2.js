d57222f4ba2da92e9d5f6da6a4fde187c082f1ce
SonoNessunk <162753958+Zefrion@users.noreply.github.com>
2024-03-08 22:29:06 +0100

    .

--- js/filter2.js
+++ js/filter2.js
@@ -1,15 +1,15 @@
 function filterVer() {
     var select, ul, li, category;
     select = document.getElementById('versionSelect');
-    ul = document.getElementById('myVer');
-    li = ul.getElementsByTagName('div');
+    ul = document.getElementsByClassName('myVer');
+    li = ul.getElementsByClassName('div');
     category = select.value;
 
 
     for (var i = 0; i < li.length; i++) {
       var dataCategory = li[i].getAttribute('ver-category');
 
-      if (category === 'tutte' || dataCategory === category) {
+      if (categoryy === 'tutte' || dataCategory === category) {
         li[i].style.display = '';
       } else {
         li[i].style.display = 'none';
